b6ebe08fb6ae31cd9e40d4f88c6c9abc8ab18fda
dddonghwa <38090151+dddonghwa@users.noreply.github.com>
2021-06-15 15:28:52 +0900

    20210615_update
    20210615_update

---
+++ class08/01_12_myScript.js
@@ -0,0 +1,4 @@
+function myFunc(){
+    document.getElementById("demo").innerHTML = "No! this is HTML! 👿"
+    document.getElementById("demo").style.color = 'red'
+}
---
+++ class08/01_14_myScript.js
@@ -0,0 +1,11 @@
+function myFunc1(){
+    a = "var"
+    b = 10
+//    document.getElementById("demo1").innerHTML = "내용 초기화1 : "+"\n"+ a + b
+    demo1.innerHTML = "이것도 되나? 되네"
+}
+
+function myFunc2(usr){
+    document.getElementById("demo2").innerHTML = "내용 초기화2 : " + usr
+//    demo2.innerHTML = "이것도 되나? 되네?"
+}
